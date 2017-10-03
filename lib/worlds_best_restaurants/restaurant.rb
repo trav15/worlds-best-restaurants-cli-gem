d6e619836d139a5f07cdb437a7c9cbe889b5e11e
cjbrock <corinna@flatironschool.com>
2017-10-03 18:31:54 -0400

    add in xpath commented out to compare, removed binding

--- lib/worlds_best_restaurants/restaurant.rb
+++ lib/worlds_best_restaurants/restaurant.rb
@@ -32,27 +32,32 @@ class WorldsBestRestaurants::Restaurant
 
   def best_dish
     @best_dish ||= doc.css("div.c-4.nr.nt ul:nth-child(8) li").text
+    # @best_dish ||= doc.xpath("//div[@class='c-4 nr nt']/ul[3]/li").text
   end
 
   def food_style
-    binding.pry
     @food_style ||= doc.css("div.c-4.nr.nt ul:nth-child(6) li").text
+    # @food_style ||= doc.xpath("//div[@class='c-4 nr nt']/ul[2]/li").text
   end
 
   def contact
     @contact ||= doc.css("div.c-4.nr.nt ul:nth-child(10) li:nth-child(1)").text.split("+").join(". Tel: +")
+    # @contact ||= doc.xpath("//div[@class='c-4 nr nt']/ul[4]/li[1]").text.split("+").join(". Tel: +")
   end
 
   def head_chef
     @head_chef ||= doc.css("div.c-4.nr.nt ul:nth-child(3) li").text.split(" (pictured)").join("")
+    # @head_chef ||= doc.xpath("//div[@class='c-4 nr nt']/ul[1]/li").text.split(" (pictured)").join("")
   end
 
   def website_url
     @website_url ||= doc.css("div.c-4.nr.nt ul:nth-child(10) li:nth-child(2) a").text
+    # @website_url ||= doc.xpath("//div[@class='c-4 nr nt']/ul[4]/li[2]/a").text
   end
 
   def description
     @description ||= doc.css("div.c-8.nl.nt > p:nth-child(6)").text
+    # @description ||= doc.xpath("//div[@class='c-8 nl nt']/p[3]").text
   end
 
   def doc
